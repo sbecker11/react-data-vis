8bf20a64367e4457adb60f1b7c2b58100b9c048e
Shawn Becker <sbecker11@me.com>
2024-09-01 08:16:44 -0600

    layout width is contrained to max width of children

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import Resume from './Resume';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Resume Visualizations</h1>
-        <p>
-          This is a project for visualizing resumes.
-        </p>
-      </header>
       <Resume/>
     </div>
   );
